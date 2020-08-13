fe1fa0ac10144de8dadb8258f59639a4e95cac6d
sreedam hembram <sreedamhembram@sreedams-MacBook-Pro.local>
2020-08-13 16:59:45 +0530

    new commit

---
+++ src/main/java/guru/springframework/msscbrewery/MsscBreweryApplication.java
@@ -0,0 +1,14 @@
+package guru.springframework.msscbrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBreweryApplication.class, args);
+	}
+	/*git remote add origin git@github.com:sreedam/mssc-brewery.git
+	git push -u origin master*/
+}
