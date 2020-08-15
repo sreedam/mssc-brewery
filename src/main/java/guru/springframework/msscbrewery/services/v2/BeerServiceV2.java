a068ad7cc1dd0f093168c655b1f2fe87df0023a5
sreedam <sreedamhembram21@gmail.com>
2020-08-16 00:48:22 +0530

    api versioning added

---
+++ src/main/java/guru/springframework/msscbrewery/services/v2/BeerServiceV2.java
@@ -0,0 +1,16 @@
+package guru.springframework.msscbrewery.services.v2;
+
+import guru.springframework.msscbrewery.web.model.BeerDto;
+import guru.springframework.msscbrewery.web.model.v2.BeerDtoV2;
+
+import java.util.UUID;
+
+public interface BeerServiceV2 {
+    BeerDtoV2 getBeerById(UUID beerId);
+
+    BeerDtoV2 saveNewBeer(BeerDtoV2 beerDtoV2);
+
+    void deleteBeer(UUID beerId);
+
+    void updateBeer(UUID beerId, BeerDtoV2 beerDtoV2);
+}
