d59c58236582db301c35162e9b0d1ea98258a691
sreedam <sreedamhembram21@gmail.com>
2020-08-15 23:46:06 +0530

    Delete operation added for Beer

--- src/main/java/guru/springframework/msscbrewery/services/BeerServiceImpl.java
+++ src/main/java/guru/springframework/msscbrewery/services/BeerServiceImpl.java
@@ -1,10 +1,12 @@
 package guru.springframework.msscbrewery.services;
 
 import guru.springframework.msscbrewery.web.model.BeerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class BeerServiceImpl implements BeerService {
     @Override
@@ -20,5 +22,10 @@ public class BeerServiceImpl implements BeerService {
     @Override
     public void updateBeer(UUID beerId, BeerDto beerDto) {
         //todo  impl would add a real impl to update beer
+    }
+
+    @Override
+    public void deleteBeer(UUID beerId) {
+        log.debug("Deleting a Beer.....");
     }
 }
